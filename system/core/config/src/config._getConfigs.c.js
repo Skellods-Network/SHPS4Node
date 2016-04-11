8847f398bbb5059d74e20831b685712916c10daa
minecrawler <marco@skellods.de>
2016-04-11 16:56:44 +0200

    Update config._getConfigs.c.js

--- system/core/config/src/config._getConfigs.c.js
+++ system/core/config/src/config._getConfigs.c.js
@@ -2,7 +2,7 @@
 
 var sym = require('../interface/config-symbols.h.js');
 
-require('../interface/config.h.js').prototype.getConfigs = function () {
+require('../interface/config.h.js').prototype._getConfigs = function () {
 
     return this[sym.cfg.vhosts];
 };
