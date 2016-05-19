eff0c4ccf81dea635ba1e7dd857582edb1483807
minecrawler <me@marco-alka.de>
2016-05-19 16:54:47 +0200

    checkin wip files

--- system/plugins/test/test.js
+++ system/plugins/test/test.js
@@ -6,15 +6,48 @@ var q = require('q');
 var libs = require('node-mod-load').libs;
 
 
-var _onDirectCall 
-= me.onDirectCall = function ($requestState) {
+me.onDirectCall = function ($requestState) {
     
     var defer = q.defer();
     
-    var langs = libs.lang.getAcceptLanguageList();
-    $requestState.responseBody = 'Hello from TEST PLUGiN<br>' + JSON.stringify(langs);
-    $requestState.responseStatus = 200;
-    defer.resolve();
+    var nosql = new libs.nosql('nedb', $requestState);
+    nosql.init().done(() => {
 
+        var table = nosql.openTable('accesskey');
+
+        table.insert({
+
+            name: 'Hellow',
+            description: 'World',
+        }).done(($res) => {
+
+            var nosql2 = new libs.nosql('nedb', $requestState);
+            nosql2.init().done(() => {
+
+                var table = nosql.openTable('accesskey');
+
+                table.insert({
+
+                    name: 'Hellow2',
+                    description: 'World2',
+                }).done(($res) => {
+
+                    $requestState.responseStatus = 200;
+                    $requestState.responseBody = JSON.stringify($res);
+                    defer.resolve();
+                }, $err => {
+
+                    $requestState.responseStatus = 500;
+                    $requestState.responseBody = $err;
+                    defer.reject($err);
+                });
+            }, defer.reject);
+        }, $err => {
+
+            $requestState.responseStatus = 500;
+            $requestState.responseBody = $err;
+            defer.reject($err);
+        });
+    }, defer.reject);
     return defer.promise;
 };
