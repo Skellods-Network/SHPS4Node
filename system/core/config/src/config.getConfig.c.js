8ffb1ccc5af1a59aca5e0bc3943189d59f0c21cf
minecrawler <me@marco-alka.de>
2016-07-29 17:41:35 +0200

    remove config
    migrated to external project

--- system/core/config/src/config.getConfig.c.js
+++
@@ -1,8 +0,0 @@
-﻿'use strict';
-
-var sym = require('../interface/config-symbols.h.js');
-
-require('../interface/config.h.js').prototype.getConfig = function ($uri) {
-
-    return this[sym.cfg.vhosts][$uri];
-};
