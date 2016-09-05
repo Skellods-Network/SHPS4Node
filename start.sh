be42707359d4057fcdfae09308a8e8397ee85d72
minecrawler <me@marco-alka.de>
2016-09-05 09:27:14 +0200

    fix start for *NIX systems

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# Untested, sry. Will do later :)
-
-NODE_COM = "node"
-command -v $NODE_COM >/dev/null 2>&1 || {
-
-  NODE_COM = "nodejs"
-  command -v $NODE_COM >/dev/null 2>&1 || {
-
-    echo >&2 "Could not find Node.  Aborting.";
-    exit 1;
-  }
-}
-
-eval "$NODE_COM SHPS.js"
-read -rsp $'Press any key to continue...\n'
+node ./SHPS.js
