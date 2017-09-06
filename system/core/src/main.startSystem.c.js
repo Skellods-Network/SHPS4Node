7cd7fec679db3f2735da49e0a549fe4b5e289100
minecrawler <me@marco-alka.de>
2017-09-06 15:57:24 +0200

    clean up start script

--- system/core/src/main.startSystem.c.js
+++ system/core/src/main.startSystem.c.js
@@ -7,9 +7,6 @@ const libs = require('node-mod-load')('SHPS4Node').libs;
 const H = require('../interface/main.h');
 
 H.prototype.startSystem = async function mainStartSystem() {
-    let task;
-    let ok = true;
-
     libs.coml.write('Establish process objective in cluster...');
     if (pc.isWorker) {
         // todo: get cluster info and register to get functionality. Do other stuff dependeing on that!
@@ -17,46 +14,89 @@ H.prototype.startSystem = async function mainStartSystem() {
         return;
     }
     else {
-        libs.coml.writeLn(' I am Master!');
+        libs.coml.writeLn(' I am the Master!');
     }
 
-    libs.coml.writeLn('Check filesystem...');
-    // todo: check FS (r+w on necessary folders, r-only on certain folders)
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: implement FS check!' });
+    {
+        libs.coml.writeLn('Check filesystem...');
+        // todo: check FS (r+w on necessary folders, r-only on certain folders)
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: implement FS check!' });
+    }
 
-    this.writeLog(this.logLevels.debug, { mod: 'MAIN', msg: `load plugins from "${libs.main.directories.plugins}"` });
-    await libs.plugin.loadPlugins(libs.main.directories.plugins);
+    {
+        // todo: decouple module by moving dependency on coml here
+        await libs.plugin.loadPlugins(libs.main.directories.plugins);
+    }
 
-    ok = true;
-    task = libs.coml.startTask('Load templates');
-    await libs.config.loadTemplates(libs.main.directories.templates, {
-        fileFoundHandler: $fileName => task.interim(task.result.ok, `Found file "${$fileName}"`),
-        fileLoadedHandler: $templateName => task.interim(task.result.ok, `Loaded template "${$templateName}"`),
-        errorHandler: ($fileName, $err) => { task.interim(task.result.error, `${$fileName}: ${$err.message}`); ok = false; },
-    });
+    {
+        const task = libs.coml.startTask('Load templates');
+        let ok = true;
 
-    task.end(ok ? task.result.ok : task.result.error);
+        await libs.config.loadTemplates(libs.main.directories.templates, {
+            fileFoundHandler: $fileName => task.interim(task.result.ok, `Found file "${$fileName}"`),
+            fileLoadedHandler: $templateName => task.interim(task.result.ok, `Loaded template "${$templateName}"`),
+            errorHandler: ($fileName, $err) => {
+                task.interim(task.result.error, `${$fileName}: ${$err.message}`);
+                ok = false;
+            },
+        });
 
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: check if master-config template is available, else download it!' });
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: load configs!' });
-    //await libs.config.loadConfigs(libs.main.directories.configs);
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: check if master-config is available, else create it!' });
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: check if master-config template is available, else download it!' });
+        task.end(ok ? task.result.ok : task.result.error);
+    }
 
-    libs.coml.writeLn('Check environment...');
-    // todo: check env (SQL)
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: check environment, e.g. SQL!' });
+    {
+        const task = libs.coml.startTask('Load settings');
+        let ok = true;
 
-    libs.coml.writeLn('Start sub-processes...');
-    // todo: start sub-processes, establish IPC, distribute tasks, etc.
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: load configs!' });
+        ok = false;
 
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: Master should not serve, only build pipeline on slaves!' });
-    libs.coml.writeLn('Load pipelines...');
-    libs.coml.writeLn('Build pipelines...');
-    libs.coml.writeLn('Start pipelines...');
-    // todo: build pipelines and start them
-    this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: build pipeline!' });
+        //await libs.config.loadConfigs(libs.main.directories.configs);
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: check if master-config is available, else create it!' });
+        task.end(ok ? task.result.ok : task.result.error);
+    }
 
-    libs.coml.writeLn('Start terminal...');
+    {
+        const task = libs.coml.startTask('Check environment');
+        let ok = true;
+
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: check environment, e.g. SQL!' });
+        ok = false;
+
+        // todo: check env (SQL)
+
+        task.end(ok ? task.result.ok : task.result.error);
+    }
+
+    {
+        const task = libs.coml.startTask('Start sub-processes');
+        let ok = true;
+
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: start sub-processes!' });
+        ok = false;
+
+        // todo: start sub-processes, establish IPC, distribute tasks, etc.
+
+        task.end(ok ? task.result.ok : task.result.error);
+    }
+
+    {
+        const task = libs.coml.startTask('Prepare pipeline');
+        let ok = true;
+
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: Master should not serve, only build pipeline on slaves!' });
+        ok = false;
+
+        // todo: build pipelines and start them
+        this.writeLog(this.logLevels.warning, { mod: 'MAIN', msg: 'fixme: build pipeline!' });
+        task.interim(task.result.error, `Loaded pipelines`);
+        task.interim(task.result.error, `Built pipelines`);
+
+        task.end(ok ? task.result.ok : task.result.error);
+    }
+
+    libs.coml.writeLn('\nStart terminal...');
     libs.coml.registerCommand('exit', 'exit', 'Shutdown SHPS', () => libs.init.halt());
     libs.coml.startInputMode();
 
